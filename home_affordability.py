e74edd6f3095f8cb2ab9bddeccaccc34069f12f7
anuveyatsu <anuar.ustayev@gmail.com>
2019-02-06 01:27:34 +0600

    [normalizarion][m]: unpivoted the data

--- home_affordability.py
+++ home_affordability.py
@@ -1,6 +1,6 @@
 import datetime
 
-from dataflows import Flow, load, dump_to_path, PackageWrapper, ResourceWrapper, printer
+from dataflows import Flow, load, dump_to_path, PackageWrapper, ResourceWrapper, printer, unpivot
 from datapackage import package
 
 
@@ -44,6 +44,29 @@ def filter_row_house_price_ratio(rows):
             yield row
 
 
+unpivot_fields = [
+    {'name': '2002', 'keys': {'Value': '2002', 'Year': '2002'}},
+    {'name': '2003', 'keys': {'Value': '2003', 'Year': '2003'}},
+    {'name': '2004', 'keys': {'Value': '2004', 'Year': '2004'}},
+    {'name': '2005', 'keys': {'Value': '2005', 'Year': '2005'}},
+    {'name': '2006', 'keys': {'Value': '2006', 'Year': '2006'}},
+    {'name': '2007', 'keys': {'Value': '2007', 'Year': '2007'}},
+    {'name': '2008', 'keys': {'Value': '2008', 'Year': '2008'}},
+    {'name': '2009', 'keys': {'Value': '2009', 'Year': '2009'}},
+    {'name': '2010', 'keys': {'Value': '2010', 'Year': '2010'}},
+    {'name': '2011', 'keys': {'Value': '2011', 'Year': '2011'}},
+    {'name': '2012', 'keys': {'Value': '2012', 'Year': '2012'}},
+    {'name': '2013', 'keys': {'Value': '2013', 'Year': '2013'}},
+    {'name': '2014', 'keys': {'Value': '2014', 'Year': '2014'}},
+    {'name': '2015', 'keys': {'Value': '2015', 'Year': '2015'}},
+    {'name': '2016', 'keys': {'Value': '2016', 'Year': '2016'}},
+    {'name': '2017', 'keys': {'Value': '2017', 'Year': '2017'}}
+]
+extra_keys = [
+    {'name': 'Year', 'type': 'any'}
+]
+extra_value = {'name': 'Value', 'type': 'any'}
+
 
 def house_price_ratio(link):
     Flow(
@@ -55,9 +78,9 @@ def house_price_ratio(link):
         sheet=2),
         filter_row_house_price_ratio,
         set_format_home_price_ratio,
-        dump_to_path('data'),
+        unpivot(unpivot_fields, extra_keys, extra_value),
+        dump_to_path('.'),
         printer(num_rows=1)
     ).process()
-
 
 house_price_ratio('https://data.london.gov.uk/download/ratio-house-prices-earnings-borough/122ea18a-cb44-466e-a314-e0c62a32529e/ratio-house-price-earnings-residence-based.xls')
